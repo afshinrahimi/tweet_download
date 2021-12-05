efec9f7a9963be167e724ba5ae578c69bd74c79f
Afshin Rahimi <afshinrahimi@gmail.com>
2021-12-05 11:08:06 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/afshinrahimi/tweet_download.git
+conda create -n ntp 
+conda activate ntp
+conda install -y tensorflow-gpu==1.13.1
+conda install -y scikit-learn==0.23.2 
